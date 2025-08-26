b3e1ef1dfe36d4f72dd5fde952ca1c0aa44ee7ba
AR-794 <athirarajeev794@gmaail.com>
2025-08-26 16:09:22 +0530

    initial commit

---
+++ mini_nav/src/amr_description/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'amr_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='athira',
+    maintainer_email='athirarajeev794@gmaail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    # tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller = amr_description.controller:main',
+        ],
+    },
+)
